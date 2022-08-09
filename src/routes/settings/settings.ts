637600a11b616d790ebd8cedbd2c04a7c3e38f62
aland <alandsazkm@gmail.com>
2022-08-10 01:33:45 +0300

    settings finished

--- src/routes/settings/settings.ts
+++ src/routes/settings/settings.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 import { findOne, insertOne, updateOne } from '../../mongo/mongo'
 import { JWT_SECRET, USER_COLLECTION } from '../../../config';
 import { sendSuccessRespose, sendFailedResponse } from '../../utils/response'
-import { User } from '../../types/user';
+import { SettingsUser as User } from '../../types/user';
 import { verifyJwt } from '../../utils/verify-jwt';
 
 const app = express();
@@ -21,15 +21,13 @@ export const settings = app.post('/settings', async (req, res) => {
         console.log('userParseErrror', e);
         return sendFailedResponse(res, 400, { message: e.issues[0].message })
     }
+    
     let findResult: User = await findOne(USER_COLLECTION, { username: token.username })
     if (!findResult) return sendFailedResponse(res, 400, { message: 'user not found' })
 
     if (findResult) {
-
         if (user.password) user.password = await bcrypt.hash(user.password, 11);
-        console.log(user);
         let updateResult = await updateOne(USER_COLLECTION, { username: findResult.username }, { $set: user })
-        console.log(updateResult);
         if (true) {
             let newUser: User = await findOne(USER_COLLECTION, { username: user.username })
             const token = jwt.sign(newUser, JWT_SECRET);
--- src/types/user.ts
+++ src/types/user.ts
@@ -4,3 +4,11 @@ export const User = z.object({
     password: z.string().max(9),
 });
 export type User = z.infer<typeof User>;
+
+export const SettingsUser = z.object({
+    username: z.string().max(9).optional(),
+    password: z.string().max(9).optional(),
+}).refine(({ username, password }) =>
+        username !== undefined || password !== undefined,
+    { message: "One of the fields must be defined" })  
+export type SettingsUser = z.infer<typeof User>;
