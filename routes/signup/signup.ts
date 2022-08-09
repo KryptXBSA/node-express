82537c1d8ddd062c42aeb3ab37fb09cffe03b201
aland <alandsazkm@gmail.com>
2022-08-09 22:52:56 +0300

    zod and password hashing

--- routes/signup/signup.ts
+++ routes/signup/signup.ts
@@ -1,5 +1,28 @@
 import express from 'express';
 const app = express();
+import { z } from "zod";
+import bcrypt from 'bcrypt'
+
+const User = z.object({
+    username: z.string().max(9),
+    password: z.string().max(9),
+});
+type User = z.infer<typeof User>;
+
 export const signupRoute = app.post('/signup', async (req, res) => {
-    res.send('signup')
+    let user: User
+    let hashedPassword: string
+    try {
+        user = User.parse(req.body);
+        console.log(user);
+    } catch (e) {
+        console.log('userParseErrror', e);
+        res.send(e.issues[0].message)
+    }
+    hashedPassword = await bcrypt.hash(user.password, 11);
+
+    const match = await bcrypt.compare(user.password, hashedPassword);
+    console.log(match);
+
+    res.send(user)
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-import { router } from './../routes/index';
+import { router } from '../routes/routes';
 import 'dotenv/config'
+
 const app = express()
 const port = process.env.PORT || 7002
+app.use(express.json())
 app.use(router)
+
 app.get('/', async (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
